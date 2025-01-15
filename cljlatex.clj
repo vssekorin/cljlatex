d94f8407c2230114042f1278cb3d290d8237b931
Vseslav Sekorin <vssekorin@gmail.com>
2025-01-15 23:28:14 +0300

    Multiline block

--- cljlatex.clj
+++ cljlatex.clj
@@ -27,14 +27,25 @@
          (str-last end))))
 
 (def lines*
-  (for [line (fs/read-all-lines in-file-name)
-        :let [start-position (str/index-of line start-symb)
-              end-position (str/index-of line end-symb)]]
-    (if (and start-position end-position)
-        (build-lines (subs line 0 start-position)
-                     (subs line (+ (count start-symb) start-position) end-position)
-                     (subs line (+ (count end-symb) end-position) (count line)))
-        line)))
+  (let [state (atom nil) start (atom nil) script (atom nil)]
+    (for [line (fs/read-all-lines in-file-name)
+          :let [start-position (str/index-of line start-symb)
+                end-position (str/index-of line end-symb)]]
+      (cond
+        (and start-position end-position) (build-lines (subs line 0 start-position)
+                                                       (subs line (+ (count start-symb) start-position) end-position)
+                                                       (subs line (+ (count end-symb) end-position) (count line)))
+        (and start-position (not end-position)) (do (reset! state :read-clj)
+                                                    (reset! start (subs line 0 start-position))
+                                                    (swap! script str (subs line (+ (count start-symb) start-position) (count line)))
+                                                    nil)
+        (and end-position (= @state :read-clj)) (do (reset! state nil)
+                                                    (build-lines @start
+                                                                 (str @script (subs line 0 end-position))
+                                                                 (subs line (+ (count end-symb) end-position) (count line))))
+        (= @state :read-clj) (do (swap! script str line)
+                                 nil)
+        :else line))))
 
 (fs/delete-if-exists out-file-name)
 (fs/create-file out-file-name)
